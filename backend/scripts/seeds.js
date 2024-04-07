a6ff29ffbbc7549c72a352897b68fe370cd227a3
soullezz <148841411+soullezz@users.noreply.github.com>
2024-04-07 06:25:42 +0000

    seed script fix

--- backend/scripts/seeds.js
+++ backend/scripts/seeds.js
@@ -1,53 +1,66 @@
-//TODO: seeds script should come here, so we'll be able to put some data in our local env
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+// Load environment variables from .env file
+dotenv.config();
+
+// Connect to MongoDB
 const connection = process.env.MONGODB_URI;
-mongoose.connect(connection);
+mongoose.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to MongoDB");
 
+    // Import Mongoose models
+    const User = require(".User");
+    const Item = require(".Item");
+    const Comment = require(".Comment");
 
-const User = mongoose.model("../models/Users");
-const Item = mongoose.model("../models/Item");
-const Comment = mongoose.model("../models/Comment");
+    // Seed the database
+    seedDatabase()
+      .then(() => {
+        console.log("Finished DB seeding");
+        process.exit(0);
+      })
+      .catch((err) => {
+        console.error(`Error while running DB seed: ${err.message}`);
+        process.exit(1);
+      });
+  })
+  .catch((err) => {
+    console.error(`Error connecting to MongoDB: ${err.message}`);
+    process.exit(1);
+  });
 
 async function seedDatabase() {
-    for (let i = 0; i < 100; i++) {
-      // add user
-      const user = { username:  `user${i}`, email: `user${i}@gmail.com` };
-      const options = { upsert: true, new: true };
-      const createdUser = await User.findOneAndUpdate(user, {}, options);
-      
-      // add item to user
-      const item = {
-        slug: `slug${i}`,
-        title: `title ${i}`,
-        description: `description ${i}`,
-        seller: createdUser,
-      };
-      const createdItem = await Item.findOneAndUpdate(item, {}, options);
-      
-      // add comments to item
-      if (!createdItem?.comments?.length) {
-        let commentIds = [];
-        for (let j = 0; j < 100; j++) {
-          const comment = new Comment({
-            body: `body ${j}`,
-            seller: createdUser,
-            item: createdItem,
-          });
-          await comment.save();
-          commentIds.push(comment._id);
-        }
-        createdItem.comments = commentIds;
-        await createdItem.save();
+  for (let i = 0; i < 100; i++) {
+    // Add user
+    const user = { username: `user${i}`, email: `user${i}@gmail.com` };
+    const options = { upsert: true, new: true };
+    const createdUser = await User.findOneAndUpdate(user, {}, options);
+
+    // Add item to user
+    const item = {
+      slug: `slug${i}`,
+      title: `title ${i}`,
+      description: `description ${i}`,
+      seller: createdUser._id,
+    };
+    const createdItem = await Item.findOneAndUpdate(item, {}, options);
+
+    // Add comments to item
+    if (!createdItem.comments || createdItem.comments.length === 0) {
+      let commentIds = [];
+      for (let j = 0; j < 100; j++) {
+        const comment = new Comment({
+          body: `body ${j}`,
+          seller: createdUser._id,
+          item: createdItem._id,
+        });
+        await comment.save();
+        commentIds.push(comment._id);
       }
+      createdItem.comments = commentIds;
+      await createdItem.save();
     }
   }
-
-seedDatabase()
-  .then(() => {
-  console.log("Finished DB seeding");
-  process.exit(0);
-})
-.catch((err) => {
-  console.log(`Error while running DB seed: ${err.message}`);
-  process.exit(1);
-});
+}
